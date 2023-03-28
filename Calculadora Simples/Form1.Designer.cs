f2c26f7b44b4f836144e82ef238b784878149954
Caroline Nogueira <carolinenogueira765@gmail.com>
2023-03-28 18:39:21 -0400

    Adicionar arquivos de projeto.

---
+++ Calculadora Simples/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Calculadora_Simples
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.comboBoxescolher = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(171, 177);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // comboBoxescolher
+            // 
+            this.comboBoxescolher.FormattingEnabled = true;
+            this.comboBoxescolher.Items.AddRange(new object[] {
+            "Somar",
+            "Subtrair",
+            "Dividir",
+            "Multiplicar"});
+            this.comboBoxescolher.Location = new System.Drawing.Point(32, 176);
+            this.comboBoxescolher.Name = "comboBoxescolher";
+            this.comboBoxescolher.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxescolher.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor 1:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor 2: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(106, 226);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(93, 20);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "Resultado";
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 268);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxescolher);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "frmCalculadora";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox comboBoxescolher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Calculadora Simples/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Simples
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+        private string Resultado(double valor1, double valor2)
+        {
+            double soma, multiplica, divide, subtrai;
+            valor1 = Convert.ToDouble(textBox1.Text);
+            valor2 = Convert.ToDouble(textBox2.Text);
+            soma = valor1 + valor2;
+            multiplica = valor1 * valor2;
+            divide = valor1 / valor2;
+            subtrai = valor1 - valor2;
+
+            if (comboBoxescolher.Text == "Somar")
+            {
+                return soma.ToString();
+            }
+            if (comboBoxescolher.Text == "Multiplicar")
+            {
+                return multiplica.ToString();
+            }
+            if (comboBoxescolher.Text == "Dividir")
+            {
+                try
+                {
+                    if (valor2 == 0)
+                    {
+                        throw new DivideByZeroException("Não é permitida a divisão por zero.");
+                    }
+                }
+                catch (DivideByZeroException ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro de cálculo");
+                }
+
+                return divide.ToString();
+            }
+                
+            
+            if (comboBoxescolher.Text == "Subtrair")
+            {
+                return subtrai.ToString();
+            }
+            else
+                return "";
+
+        }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2;
+
+            if (!Double.TryParse(textBox1.Text, out valor1))
+            {
+                MessageBox.Show("O primeiro número não é válido. Insira um número válido.", "Erro de validação");
+                return;
+            }
+
+            if (!Double.TryParse(textBox2.Text, out valor2))
+            {
+                MessageBox.Show("O segundo número não é válido. Insira um número válido.", "Erro de validação");
+                return;
+            }
+            lblTotal.Text = Resultado(0, 0).ToString();
+        }
+    }
+}
